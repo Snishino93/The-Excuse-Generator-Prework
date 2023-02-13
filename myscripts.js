6d1df398564b8e354b4ddc16ee5e190b8977ed24
Soraran6 <shiki1893@gmail.com>
2023-02-13 19:59:58 +0000

    First commit

---
+++ myscripts.js
@@ -0,0 +1,14 @@
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+function getExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+  return "Sorry, I can't make it to the meeting today because " + randomWho + " " + randomWhat + " my homework " + randomWhen + ".";
+}
+
+window.onload = function() {
+  document.getElementById("excuse").innerHTML = getExcuse();
+}
